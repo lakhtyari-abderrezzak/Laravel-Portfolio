35f74ab74cd5e2468df3e31a8bd6c86756f3ff37
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-18 19:12:49 +0000

    added articles And Show articles Route to web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,17 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index');
+    return view('home');
 })->name('home');
+
+Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
+Route::get('articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
+
+// Contact Form Submition Route
 
 Route::middleware([
     'auth:sanctum',
