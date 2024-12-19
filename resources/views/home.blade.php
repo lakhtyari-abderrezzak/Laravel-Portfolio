68d9af30a291386685ef5e00f8cdc74449480ed1
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-19 01:10:35 +0000

    Added x-about to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,8 +2,12 @@
     
     {{-- landing page --}}
     <x-partials.landing />
+    <x-partials.about />
+    <div class="spikes"></div>
+    {{-- Testemonials --}}
+    <livewire:testemonial-box />
     {{-- contact section  --}}
     <x-partials.contact />
-
+    
 
 </x-app-layout>
