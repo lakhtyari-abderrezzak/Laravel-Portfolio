b6ba868438c727444f10975562909b4cf687036f
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-21 23:20:28 +0000

    Included Skills, Caroussel and Spikes to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,13 +1,17 @@
 <x-app-layout>
-    
+
     {{-- landing page --}}
     <x-partials.landing />
     <x-partials.about />
     <div class="spikes"></div>
     {{-- Testemonials --}}
+    <x-partials.skills />
+    <x-caroussel />
+
+    <div class="spikes"></div>
     <livewire:testemonial-box />
     {{-- contact section  --}}
     <x-partials.contact />
-    
+
 
 </x-app-layout>
