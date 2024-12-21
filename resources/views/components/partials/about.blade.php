bac302c728538c749627b6448f1024bfe04f192c
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-21 23:08:46 +0000

    Deleted Caroussel div From About Section

--- resources/views/components/partials/about.blade.php
+++ resources/views/components/partials/about.blade.php
@@ -21,13 +21,4 @@
             here to help bring your vision to life.</p>
     </div>
 </section>
-<div class="carossel">
-    <div class="item item1"><img src="{{asset('images/svg/bootstrap.svg')}}" alt=""></div>
-    <div class="item item2"><img src="{{asset('images/svg/css-3.svg')}}" alt=""></div>
-    <div class="item item3"><img src="{{asset('images/svg/html-5.svg')}}" alt=""></div>
-    <div class="item item4"><img src="{{asset('images/svg/javascript.svg')}}" alt=""></div>
-    <div class="item item5"><img src="{{asset('images/svg/laravel.svg')}}" alt=""></div>
-    <div class="item item6"><img src="{{asset('images/svg/php.svg')}}" alt=""></div>
-    <div class="item item7"><img src="{{asset('images/svg/tailwindcss.svg')}}" alt=""></div>
-    <div class="item item8"><img src="{{asset('images/svg/mysql.svg')}}" alt=""></div>
-</div>
+
