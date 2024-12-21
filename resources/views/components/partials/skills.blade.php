e6b25d08a2f912a498d2585fd9f07497455558a3
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-21 23:11:22 +0000

    created Skills blade in component folder

---
+++ resources/views/components/partials/skills.blade.php
@@ -0,0 +1,20 @@
+    <!-- Start Skills -->
+    <div class="our-skills" id="our-skills">
+        <h2 class="main-title">Skills</h2>
+        <div class="container">
+            <img src="{{ asset('images/skills.png') }}" alt="skills image">
+            <div class="skills">
+                <x-skill icon="html5" name="HTML" percentage="99%" />
+                <x-skill icon="css3" name="CSS" percentage="99%" />
+                <x-skill icon="bootstrap" name="Bootstrap" percentage="70%" />
+                <x-skill icon="js" name="Javascript" percentage="90%" />
+                <x-skill icon="react" name="React" percentage="80%" />
+                <x-skill icon="php" name="PHP" percentage="90%" />
+                <x-skill icon="laravel" name="Laravel" percentage="95%" />
+                <x-skill icon="mysql" name="Mysql" percentage="95%" />
+            </div>
+        </div>
+        
+    </div>
+    
+    <!-- End Skills -->
