64c3cb3031308161043669679447b2c5bc8c90a5
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-24 15:06:13 +0000

    Added Statistics section to components

---
+++ resources/views/components/partials/stats.blade.php
@@ -0,0 +1,22 @@
+<div class="container">
+    <div class="box">
+      <i class="far fa-user fa-2x fa-fw"></i>
+      <span class="number" data-goal="30">0</span>
+      <span class="text">Clients</span>
+    </div>
+    <div class="box">
+      <i class="fas fa-code fa-2x fa-fw"></i>
+      <span class="number" data-goal="100">0</span>
+      <span class="text">Projects</span>
+    </div>
+    <div class="box">
+      <i class="fas fa-globe-asia fa-2x fa-fw"></i>
+      <span class="number" data-goal="4">0</span>
+      <span class="text">Countries</span>
+    </div>
+    <div class="box">
+      <i class="far fa-money-bill-alt fa-2x fa-fw"></i>
+      <span class="number" data-goal="500">0</span>
+      <span class="text">Money</span>
+    </div>
+  </div>
