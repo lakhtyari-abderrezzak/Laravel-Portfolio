aa6431a68d80f3e186e56e4b1ce7124451148f4b
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-24 11:45:41 +0000

    Inlcluded services Section to home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,6 +4,13 @@
     <x-partials.landing />
     <x-partials.about />
     <div class="spikes"></div>
+    <!-- Start Services  -->
+    <div class="services" id="services">
+        <h2 class="main-title">Services</h2>
+        <x-partials.services />
+    </div>
+    <div class="spikes"></div>
+    <!-- End Services  -->
     {{-- Testemonials --}}
     <x-partials.skills />
     <x-caroussel />
