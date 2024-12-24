5b444eb535044000884aadf5df07c3a60661b97f
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-24 23:25:05 +0000

    Added some links and modified others and deleted unnecessary links

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -4,8 +4,8 @@
       <ul class="main-nav">
           <li><a href="{{route('home')}}">Home</a></li>
           <li><a href="{{ route('home') }}#about">About</a></li>
+          <li><a href="{{ route('home') }}#projects">Projects</a></li>
           <li><a href="{{ route('home') }}#our-skills">Skills</a></li>
-          <li><a href="{{ route('home') }}#contact">Contact</a></li>
           <li class="other-links">
               <a>Other Links</a>
           <!-- Start Megamenu -->
@@ -15,18 +15,11 @@
               </div>
               <ul class="links">
                   <li><a href="{{ route('home') }}#testemonials"><i class="far fa-comments fa-fw"></i>Testemonials</a></li>
-                  <li><a href="{{ route('articles.index')}}"><i class="fa-regular fa-newspaper"></i>Articles</a></li>
-                  <li><a href="#team members"><i class="far fa-user fa-fw"></i>Team Members</a></li>
-                  <li><a href="#services"><i class="far fa-building fa-fw"></i>Services</a></li>
-                  <li><a href="#work-steps"><i class="far fa-clipboard fa-fw"></i>How Tt Works</a></li>
-              </ul> 
-              <ul class="links">
-                  <li><a href="#events"><i class="far fa-calendar-alt fa-fw"></i>Events</a></li>
-                  <li><a href="#pricing-plan"><i class="fa regular fa-server"></i>Pricing Plans</a></li>
-                  <li><a href="#videos"><i class="far fa-play-circle fa-fw"></i>Top Videos</a></li>
                   <li><a href="{{ route('home') }}#stats"><i class="far fa-chart-bar fa-fw"></i>Stats</a></li>
-                  <li><a href="#discount"><i class="far fa-percent fa-fw"></i>Request A Discount</a></li>
-              </ul>
+                  <li><a href="{{ route('articles.index')}}"><i class="fa-regular fa-newspaper"></i>Articles</a></li>
+                  <li><a href="{{route('home')}}#services"><i class="far fa-building fa-fw"></i>Services</a></li>
+                  <li><a href="{{ route('home') }}#contact"><i class="far fa-user fa-fw"></i>Contact us</a></li>
+                </ul> 
               </ul>
           <!-- End Megamenu -->
           </li>
