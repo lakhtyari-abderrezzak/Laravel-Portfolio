64a457c2a8e963a5e21d8599762cdbb44eea31ca
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-24 11:42:48 +0000

    Added Services blade component

---
+++ resources/views/components/partials/services.blade.php
@@ -0,0 +1,8 @@
+<div class="container">
+    <x-service name="Frontend Development" img='frontend.jpg'/>
+    <x-service name="Backend Development" img='back end.jpg'/>
+    <x-service name="Full-Stack Development" img='fullstack.png'/>
+    <x-service name="Performance Optimization" img='performance.jpg'/>
+    <x-service name="Maintenance & Support" img='support.jpg'/>
+    <x-service name="Consultation & Strategy" img='consulting.jpg'/>
+</div>
