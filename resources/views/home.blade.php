343c63ff2cd64470835802a2c7bda15a2068628e
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-24 23:26:10 +0000

    Rearranged the position of some sections

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,29 +2,32 @@
 
     {{-- landing page --}}
     <x-partials.landing />
+    {{-- About section --}}
     <x-partials.about />
-    <!-- Projects Section -->
+    {{-- Projects Section  --}}
     <section id="projects" class="projects">
         <h2 class="main-title">My Projects</h2>
         <x-partials.projects />
-
     </section>
-    <!-- Start Services  -->
+    {{-- Skills  --}}
+    <x-partials.skills />
+    {{-- Caroussel  --}}
+    <x-caroussel />
+    {{-- Start Services --}}
     <div class="services" id="services">
         <h2 class="main-title">Services</h2>
         <x-partials.services />
     </div>
-    <!-- End Services  -->
-    <x-partials.skills />
-    <x-caroussel />
+    {{-- Statistics  --}}
     <div class="stats" id="stats">
         <h1 class="main-title">Stats</h1>
         <x-partials.stats />
     </div>
-    {{-- Testemonials --}}
-    <livewire:testemonial-box />
     {{-- contact section  --}}
     <x-partials.contact />
+    <div class="spikes"></div>
+    {{-- Testemonials --}}
+    <livewire:testemonial-box />
 
 
 
