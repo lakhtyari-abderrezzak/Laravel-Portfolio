dfaacc7b06f5f0e469ef9d384374e9bfd52b2bfa
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-24 23:22:20 +0000

    Edited logo and Added Some Links and Added Iframe for location

--- resources/views/layouts/partials/footer.blade.php
+++ resources/views/layouts/partials/footer.blade.php
@@ -1,70 +1,70 @@
     <!-- Start Footer -->
     <div class="footer">
-      <div class="container">
-        <div class="box">
-          <h3>Hizo</h3>
-          <ul class="social">
-            <li>
-              <a href="#" class="facebook">
-                <i class="fab fa-facebook-f"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="twitter">
-                <i class="fab fa-twitter"></i>
-              </a>
-            </li>
-            <li>
-              <a href="#" class="youtube">
-                <i class="fab fa-youtube"></i>
-              </a>
-            </li>
-          </ul>
-          <p class="text">
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus nulla rem, dignissimos iste aspernatur
-          </p>
-        </div>
-        <div class="box">
-          <ul class="links">
-            <li><a href="#">Important Link 1</a></li>
-            <li><a href="#">Important Link 2</a></li>
-            <li><a href="#">Important Link 3</a></li>
-            <li><a href="#">Important Link 4</a></li>
-            <li><a href="#">Important Link 5</a></li>
-          </ul>
-        </div>
-        <div class="box">
-          <div class="line">
-            <i class="fas fa-map-marker-alt fa-fw"></i>
-            <div class="info">Egypt, Giza, Inside The Sphinx, Room Number 220</div>
-          </div>
-          <div class="line">
-            <i class="far fa-clock fa-fw"></i>
-            <div class="info">Business Hours: From 10:00 To 18:00</div>
-          </div>
-          <div class="line">
-            <i class="fas fa-phone-volume fa-fw"></i>
-            <div class="info">
-              <span>+353831119128</span>
-              <span>+212641277204</span>
+        <div class="container">
+            <div class="box">
+                <h3> &lt; Hizo /&gt; </h3>
+                <ul class="social">
+                    <li>
+                        <a href="#" class="facebook">
+                            <i class="fab fa-facebook-f"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="twitter">
+                            <i class="fab fa-twitter"></i>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="youtube">
+                            <i class="fab fa-youtube"></i>
+                        </a>
+                    </li>
+                </ul>
+                <p class="text">
+
+                </p>
             </div>
-          </div>
+            <div class="box">
+                <ul class="links">
+                    <li><a href="#landing">Home</a></li>
+                    <li><a href="#skills">About</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#services">Services</a></li>
+                    <li><a href="#our-skills">Skills</a></li>
+                </ul>
+            </div>
+            <div class="box">
+                <div class="line">
+                    <i class="fas fa-map-marker-alt fa-fw"></i>
+                    <div class="info">Dublin, Co Dublin, Ireland</div>
+                </div>
+                <div class="line">
+                    <i class="far fa-clock fa-fw"></i>
+                    <div class="info">Business Hours: From 10:00 To 18:00</div>
+                </div>
+                <div class="line">
+                    <i class="fas fa-phone-volume fa-fw"></i>
+                    <div class="info">
+                        <span>+353831119128</span>
+                    </div>
+                </div>
+            </div>
+            <!-- Google Map Embed -->
+            <div class="footer-map">
+                <h3>Our Location</h3>
+                <!-- Embed Google Map -->
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2384.3303996974487!2d-6.248410584264696!3d53.349805380158975!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x485b5b3e4b9450f1%3A0x0f5146489d944b1e!2sDublin%2C%20Ireland!5e0!3m2!1sen!2sus!4v1634814889600!5m2!1sen!2sus"
+                     allowfullscreen="" loading="lazy"></iframe>
+
+            </div>
         </div>
-        <div class="box footer-gallery">
-          <img src="images/gallery-01.png" alt="" />
-          <img src="images/gallery-02.png" alt="" />
-          <img src="images/gallery-03.jpg" alt="" />
-          <img src="images/gallery-04.png" alt="" />
-          <img src="images/gallery-05.jpg" alt="" />
-          <img src="images/gallery-06.png" alt="" />
-        </div>
-      </div>
-      <p class="copyright">Made With Love <span>&lt;3</span> By Abdo </p>
-  </div>
-  <!-- End Footer -->
+        <p class="copyright">Made With Love <span>&lt;3</span> By Abdo </p>
+    </div>
+    <!-- End Footer -->
 
 
-  <!-- Start Up Button-->
-  <span class="go-up show"><i class="fas fa-angle-double-up fa-2x"></i></span>
-  <!-- End Up Button-->
-  <script src="{{asset('javascript/main.js')}}"></script>
+    <!-- Start Up Button-->
+    <span class="go-up "><i class="fas fa-angle-double-up fa-2x"></i></span>
+    <!-- End Up Button-->
+    <script src="{{ asset('javascript/main.js') }}"></script>
