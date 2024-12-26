4389d345516bb8268d23ec1eeca00cca2718b715
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-26 12:15:13 +0000

    Added lang file With en

---
+++ resources/lang/en/nav.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'home' => 'Home',
+    'about' => 'About',
+    'projects' => 'Projects',
+    'skills' => 'Skills',
+    'other-links' => 'Other Links',
+    'testemonials' => 'Testemonials',
+    'stats' => 'Stats',
+    'articles' => 'Articles',
+    'services' => 'Services',
+    'contact-us' => 'Contact us',
+];
