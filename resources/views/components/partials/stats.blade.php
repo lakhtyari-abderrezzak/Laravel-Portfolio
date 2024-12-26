da1071c26f5f6ad2932fae5440c08bd8931c0b5b
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-26 19:30:45 +0000

    Added global.stats to stats blade component

--- resources/views/components/partials/stats.blade.php
+++ resources/views/components/partials/stats.blade.php
@@ -2,21 +2,21 @@
     <div class="box">
       <i class="far fa-user fa-2x fa-fw"></i>
       <span class="number" data-goal="30">0</span>
-      <span class="text">Clients</span>
+      <span class="text">{{ __('global.stats.client') }}</span>
     </div>
     <div class="box">
       <i class="fas fa-code fa-2x fa-fw"></i>
       <span class="number" data-goal="100">0</span>
-      <span class="text">Projects</span>
+      <span class="text">{{ __('global.stats.projects') }}</span>
     </div>
     <div class="box">
       <i class="fas fa-globe-asia fa-2x fa-fw"></i>
       <span class="number" data-goal="4">0</span>
-      <span class="text">Countries</span>
+      <span class="text">{{ __('global.stats.countries') }}</span>
     </div>
     <div class="box">
       <i class="far fa-money-bill-alt fa-2x fa-fw"></i>
       <span class="number" data-goal="500">0</span>
-      <span class="text">Money</span>
+      <span class="text">{{ __('global.stats.money') }}</span>
     </div>
   </div>
