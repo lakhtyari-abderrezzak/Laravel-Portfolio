3db9a79e9da1ce341bdeb495d11110173c8fe810
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-26 17:57:48 +0000

    Added dynamic title and description to project.blade

--- resources/views/components/partials/projects.blade.php
+++ resources/views/components/partials/projects.blade.php
@@ -1,32 +1,32 @@
 <div class="container">
         <x-project-card  
         image="blog_website.png" 
-        title="Blog Web site" 
-        description="Laravel livewire Blog website" 
+        title="{{ __('projects.first-project.title')}}" 
+        description="{{ __('projects.first-project.description')}}" 
         github="https://github.com/lakhtyari-abderrezzak/Abdo-Blog-Website.git" />
         <x-project-card  
         image="dublin_coding.png" 
-        title="final project" 
-        description="Dublin Coding School final project using php" 
+        title="{{ __('projects.second-project.title')}}" 
+        description="{{ __('projects.second-project.description')}}" 
         github="" />
         <x-project-card  
         image="E-commerce-php.png" 
-        title="Ecommerce Website" 
-        description="Php E-commerce W website" 
+        title="{{ __('projects.third-project.title')}}" 
+        description="{{ __('projects.third-project.description')}}" 
         github="https://github.com/lakhtyari-abderrezzak/E-Commerce.git" />
         <x-project-card  
         image="fruitikha-laravel.png" 
-        title="Fruitikha" 
-        description="Laravel Fuitikha Website" 
+        title="{{ __('projects.fourth-project.title')}}" 
+        description="{{ __('projects.fourth-project.description')}}"  
         github="https://github.com/lakhtyari-abderrezzak/Fruitikha.git" />
         <x-project-card  
         image="H&L_gifts.png" 
-        title="H&L Gifts" 
-        description="Laravel H&L gifts online Shop" 
+        title="{{ __('projects.fifth-project.title')}}" 
+        description="{{ __('projects.fifth-project.description')}}" 
         github="https://github.com/lakhtyari-abderrezzak/H-L-Gifts.git" />
         <x-project-card  
         image="restaurant-management.png" 
-        title="Restaurant Managemnet" 
-        description="Restaurant Management App using Laravel & Bootstrap" 
+        title="{{ __('projects.sixth-project.title')}}" 
+        description="{{ __('projects.sixth-project.description')}}"  
         github="https://github.com/lakhtyari-abderrezzak/Restaurant-Management-System.git" />
 </div>
