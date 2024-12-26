e00f73cf236bf5e14210ac328f2e4aabe9ec2b91
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-26 12:17:29 +0000

    Changed nav hard coded titles to en  dynamic titles

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -2,23 +2,23 @@
   <div class="container">
       <x-application-logo/>
       <ul class="main-nav">
-          <li><a href="{{route('home')}}">Home</a></li>
-          <li><a href="{{ route('home') }}#about">About</a></li>
-          <li><a href="{{ route('home') }}#projects">Projects</a></li>
-          <li><a href="{{ route('home') }}#our-skills">Skills</a></li>
+          <li><a href="{{route('home')}}">{{ __('nav.home') }}</a></li>
+          <li><a href="{{ route('home') }}#about">{{ __('nav.about') }}</a></li>
+          <li><a href="{{ route('home') }}#projects">{{ __('nav.projects') }}</a></li>
+          <li><a href="{{ route('home') }}#our-skills">{{ __('nav.skills') }}</a></li>
           <li class="other-links">
-              <a>Other Links</a>
+              <a> {{ __('nav.other-links')}} </a>
           <!-- Start Megamenu -->
           <ul class="mega-menu">
               <div class="image">
                   <img src="{{asset('images/megamenu.png')}}" alt="">
               </div>
               <ul class="links">
-                  <li><a href="{{ route('home') }}#testemonials"><i class="far fa-comments fa-fw"></i>Testemonials</a></li>
-                  <li><a href="{{ route('home') }}#stats"><i class="far fa-chart-bar fa-fw"></i>Stats</a></li>
-                  <li><a href="{{ route('articles.index')}}"><i class="fa-regular fa-newspaper"></i>Articles</a></li>
-                  <li><a href="{{route('home')}}#services"><i class="far fa-building fa-fw"></i>Services</a></li>
-                  <li><a href="{{ route('home') }}#contact"><i class="far fa-user fa-fw"></i>Contact us</a></li>
+                  <li><a href="{{ route('home') }}#testemonials"><i class="far fa-comments fa-fw"></i>{{ __('nav.testemonials') }}</a></li>
+                  <li><a href="{{ route('home') }}#stats"><i class="far fa-chart-bar fa-fw"></i>{{ __('nav.stats') }}</a></li>
+                  <li><a href="{{ route('articles.index')}}"><i class="fa-regular fa-newspaper"></i>{{ __('nav.articles') }}</a></li>
+                  <li><a href="{{route('home')}}#services"><i class="far fa-building fa-fw"></i>{{ __('nav.services') }}</a></li>
+                  <li><a href="{{ route('home') }}#contact"><i class="far fa-user fa-fw"></i>{{ __('nav.contact-us') }}</a></li>
                 </ul> 
               </ul>
           <!-- End Megamenu -->
