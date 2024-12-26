e2abfedb8a386fba22a9752d5c2b64296ef80f74
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-26 17:56:51 +0000

    english lang added to projects file

--- resources/lang/en/projects.php
+++ resources/lang/en/projects.php
@@ -1,5 +1,29 @@
 <?php 
 
 return [
-
+    'title' => 'Projects',
+    'first-project' => [
+        'title'=>"Blog Web site" ,
+        'description'=>"Laravel livewire Blog website." 
+    ],
+    'second-project' => [
+        'title'=>"final project" ,
+        'description'=>"Dublin Coding School final project using php." 
+    ],
+    'third-project' => [
+        'title'=>"E-commerce Website" ,
+        'description'=>"E-commerce Website Using PHP, Bootsrtap and JQuery. " 
+    ],
+    'fourth-project' => [
+        'title'=>"Fruitikha" ,
+        'description'=>"Fruitikha is an online shop for buying orginic fruit made using Laravel and Bootstrap." 
+    ],
+    'fifth-project' => [
+        'title'=> "H and L Gifts" ,
+        'description'=>"An online  store for buying diffrent types of gifts Made Using Laravel." 
+    ],
+    'sixth-project' => [
+        'title'=> "Restaurant Management App" ,
+        'description'=> "An app that is used to Manage restaurant sales, tables and personnel. Made using Laravel." 
+    ],
 ];
