fa9954d66b04b0a88687e3df4cfc70343c0dfbe0
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-26 11:44:12 +0000

    Added Download button to landing page

--- resources/views/components/partials/landing.blade.php
+++ resources/views/components/partials/landing.blade.php
@@ -5,9 +5,13 @@
                 <h1 id="random-heading">Welcome to Abderrazzak Lakhtyari's Portfolio</h1>
                 <p>Hi, I’m Abderrazzak Lakhtyari, a passionate and experienced Full-Stack PHP Developer with expertise
                     in building robust web applications using Laravel.</p>
+                <div class="btn">
+                    <i class="fa-solid fa-download"></i>
+                    <a href="resume/full-stack-resume.pdf" download="Abderrazzak-Lakhtyari.pdf" class="download-btn">Download CV</a>
+                </div>
             </div>
             <div class="image">
-                <img id="landing-img" class="h-400" src="{{asset('images/landing-image.png')}}" alt="">
+                <img id="landing-img" class="h-400" src="{{ asset('images/landing-image.png') }}" alt="">
             </div>
         </div>
 
