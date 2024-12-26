93236b90a83b61ba0c782f5c5dd6fd546546db1d
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-26 18:19:53 +0000

    global lang added with skills and services array

---
+++ resources/lang/en/global.php
@@ -0,0 +1,17 @@
+<?php 
+
+return [
+    'skills' => [
+        'title' => 'Skills',
+    ],
+    'services' => [
+        'title' => 'Services',
+        'first' => 'Frontend Development',
+        'second' => 'Backend Development',
+        'third' => 'Full-Stack Development',
+        'fifth' => 'Performance Optimization',
+        'fourth' => 'Maintenance & Support',
+        'sixth' => 'Consultation & Strategy',
+    ]
+
+];
