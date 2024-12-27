8d4a5a7a57e05b5f83d61f9e083bc4ae910fd330
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-27 18:09:38 +0000

    Added ar, fr and sp Langauage files with about.php files

---
+++ resources/lang/fr/about.php
@@ -0,0 +1,34 @@
+<?php 
+
+return [
+    'title' => 'À propos',
+    'first-para' => [
+        'part-one' => 'Bonjour, je suis ',
+        'strong-one' => 'Abderrazzak Lakhtyari',
+        'part-two' => 'un passionné de',
+        'strong-two' => 'Développeur Web Full Stack',
+        'part-three' => 'avec une expertise tant en frontend qu’en backend. Je me spécialise dans la création d’applications web dynamiques et réactives offrant une expérience utilisateur exceptionnelle.',
+    ],
+    'second-para' => [
+        'part-one' => 'Avec une solide base en',
+        'strong-one' => 'HTML',
+        'strong-one-' => 'CSS',
+        'part-two' => 'et',
+        'strong-two' => 'JavaScript',
+        'part-three' => 'je donne vie aux sites web avec un code propre et sémantique. J’exploite la puissance de ',
+        'strong-three' => 'React',
+        'part-four' => ' pour construire des interfaces utilisateur interactives et évolutives, en garantissant des performances et une utilisabilité sur tous les appareils. Côté backend, je travaille avec ',
+        'strong-five' => 'PHP ',
+        'part-six' => 'et le framework ',
+        'strong-six' => 'Laravel ',
+        'part-seven' => 'pour créer des applications robustes et efficaces, tout en intégrant des bases de données pour garantir une gestion fluide des données.',
+    ],
+    'third-para' => [
+        'part-one' => 'Mon approche du développement est toujours centrée sur l’utilisateur, en mettant l’accent sur un design épuré, une navigation intuitive et l’optimisation des performances. Je suis un fervent défenseur du',
+        'strong-one' => 'design réactif',
+        'part-two' => 'et des principes',
+        'strong-two' => 'mobile-first',
+        'part-three' => ', garantissant que chaque application que je construis soit belle et fonctionnelle sur n’importe quelle taille d’écran.',
+    ],
+    'fourth-para' => 'Je suis toujours en quête d’apprentissage de nouvelles technologies, de résolution de défis complexes et de contribution à des projets significatifs. Que vous ayez besoin d’un site web personnel, d’une plateforme e-commerce ou d’une application web complète, je suis là pour aider à réaliser votre vision.',
+];
---
+++ resources/lang/sp/about.php
@@ -0,0 +1,34 @@
+<?php 
+
+return [
+    'title' => 'Acerca de',
+    'first-para' => [
+        'part-one' => 'Hola, soy ',
+        'strong-one' => 'Abderrazzak Lakhtyari',
+        'part-two' => 'un apasionado',
+        'strong-two' => 'Desarrollador Web Full Stack',
+        'part-three' => 'con experiencia en tecnologías tanto de frontend como de backend. Me especializo en crear aplicaciones web dinámicas y receptivas que brindan una experiencia de usuario excepcional.',
+    ],
+    'second-para' => [
+        'part-one' => 'Con una sólida base en',
+        'strong-one' => 'HTML',
+        'strong-one-' => 'CSS',
+        'part-two' => 'y',
+        'strong-two' => 'JavaScript',
+        'part-three' => 'doy vida a los sitios web con un código limpio y semántico. Aprovecho el poder de ',
+        'strong-three' => 'React',
+        'part-four' => ' para construir interfaces de usuario interactivas y escalables, asegurando un rendimiento y usabilidad en todos los dispositivos. En el backend, trabajo con ',
+        'strong-five' => 'PHP ',
+        'part-six' => 'y el framework ',
+        'strong-six' => 'Laravel ',
+        'part-seven' => 'para construir aplicaciones robustas y eficientes, integrando bases de datos para asegurar una gestión fluida de los datos.',
+    ],
+    'third-para' => [
+        'part-one' => 'Mi enfoque en el desarrollo siempre es centrado en el usuario, con énfasis en un diseño limpio, navegación intuitiva y optimización del rendimiento. Soy un firme defensor del',
+        'strong-one' => 'diseño responsivo',
+        'part-two' => 'y los principios de',
+        'strong-two' => 'mobile-first',
+        'part-three' => ', asegurando que cada aplicación que construyo luzca y funcione perfectamente en cualquier tamaño de pantalla.',
+    ],
+    'fourth-para' => 'Siempre estoy buscando aprender nuevas tecnologías, resolver desafíos complejos y contribuir a proyectos significativos. Ya sea que necesites un sitio web personal, una plataforma de comercio electrónico o una aplicación web a gran escala, estoy aquí para ayudar a llevar tu visión a la realidad.',
+];
