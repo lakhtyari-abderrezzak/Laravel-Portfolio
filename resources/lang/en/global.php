31cd6d0f74c0a81e7df405aab744a0eeb1d887b0
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-27 17:52:22 +0000

    footer en added to lang global file

--- resources/lang/en/global.php
+++ resources/lang/en/global.php
@@ -19,5 +19,22 @@ return [
         'projects' => 'Projects',
         'money' => 'Money',
         'countries' => 'Countries',
+    ],
+    'contact' => [
+        'title-side' => 'Get In Touch',
+        'description' => "I'm always open to new opportunities and collaborations. Whether you have a project in mind, need technical advice, or just want to chat about web development, feel free to reach out. I’d love to hear from you!",
+        'title' => 'Contact Me',
+        'name' => 'Enter Your Name',
+        'email' => 'Enter Your Email',
+        'number' => 'Enter Your Phone',
+        'message' => 'Tell Us About Your Needs ?',
+        'submit' => 'Submit',
+    ],
+    'footer' => [
+        'location' => 'Our Location',
+        'copyright' => 'Made With Love By Abdo',
+        'business-hours' => 'Business Hours: From 10:00 To 18:00',
+        'address' => 'Dublin, Co Dublin, Ireland'
     ]
+    
 ];
