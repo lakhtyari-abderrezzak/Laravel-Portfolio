3b27c4807c19a77f883ae04eec08ae6a85093fe9
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-27 22:53:23 +0000

    Conditionally set dir attribute to RTL for Arabic locale

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,6 +1,6 @@
 @props(['title'])
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }} " dir="{{ app()->getLocale() == 'ar' ? 'rtl' : 'ltr' }}">
 
 <head>
     <meta charset="utf-8">
