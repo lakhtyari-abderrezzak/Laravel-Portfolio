f584d7dc87a7a849545b4825fccee8587bd8d6d8
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-27 22:47:58 +0000

    Add route to set locale in session for language switcher

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,21 @@
 
 use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\ContactController;
+use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
 })->name('home');
+
+Route::get('/lang/{locale}', function ($locale) {
+    // Check if the requested language is available
+    if (in_array($locale, ['en', 'fr', 'es', 'ar'])) {
+        App::setLocale($locale);
+        session()->put('locale', $locale);  // Store the selected language in the session
+    }
+    return redirect()->back();  // Redirect back to the previous page
+});
 
 Route::get('articles', [ArticleController::class, 'index'])->name('articles.index');
 Route::get('articles/{article}', [ArticleController::class, 'show'])->name('articles.show');
