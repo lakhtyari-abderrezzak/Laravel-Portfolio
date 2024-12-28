4fb535aee0b7981c32d05bd99b39b3123139a2a9
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-28 11:43:46 +0000

    Removed Seed artical factory from database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Article::factory()->count(12)->create();
+        
 
         // User::factory()->create([
         //     'name' => 'Test User',
