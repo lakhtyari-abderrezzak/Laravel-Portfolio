5c642733427c8633d2ed0e0943cf162dd3324bc1
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2024-12-28 12:00:56 +0000

    Modified link in navigation menu

--- resources/views/navigation-menu.blade.php
+++ resources/views/navigation-menu.blade.php
@@ -4,15 +4,16 @@
         <li><a href="{{ route('home') }}">{{ __('nav.home') }}</a></li>
         <li><a href="{{ route('home') }}#about">{{ __('nav.about') }}</a></li>
         <li><a href="{{ route('home') }}#projects">{{ __('nav.projects') }}</a></li>
-        <li><a href="{{ route('home') }}#our-skills">{{ __('nav.skills') }}</a></li>
         <li class="other-links">
             <a> {{ __('nav.other-links') }} </a>
             <!-- Start Megamenu -->
             <ul class="mega-menu">
                 <div class="image">
-                    <img src="{{ asset('images/megamenu.png') }}" alt="">
+                    <img src="{{ asset('images/megamenu.png') }}" alt="mega-menu image">
                 </div>
                 <ul class="links">
+                    <li><a href="{{ route('home') }}#our-skills"><i
+                                class="fa-solid fa-kitchen-set"></i>{{ __('nav.skills') }}</a></li>
                     <li><a href="{{ route('home') }}#testemonials"><i
                                 class="far fa-comments fa-fw"></i>{{ __('nav.testemonials') }}</a></li>
                     <li><a href="{{ route('home') }}#stats"><i
@@ -26,7 +27,7 @@
             </ul>
             <!-- End Megamenu -->
         </li>
-        
+
 
     </ul>
     <!-- resources/views/layouts/app.blade.php -->
