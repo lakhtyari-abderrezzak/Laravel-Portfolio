8a8901618f801ddada02828691a1fe6cbe6cea22
Lakhtayri Abderrazzak <Lakhtyari.abderrazzak@gmail.com>
2025-03-01 12:13:24 +0000

    Clean up

--- resources/views/livewire/testemonial-box.blade.php
+++ resources/views/livewire/testemonial-box.blade.php
@@ -1,4 +1,5 @@
 <!-- Start testimonials -->
+@if (!empty($this->testemonials))
 <div class="testemonials" id="testemonials">
     <h2 class="main-title"> {{ __('global.testemonials.title') }} </h2>
     <div class="container">
@@ -6,7 +7,6 @@
             <div class="box">
                 <img src="{{ asset('images/avatar-01.png') }}" alt="testemony image">
                 <h3>{{ $testemony->name }}</h3>
-                <span>{{ $testemony->phone }}</span>
                 <div class="rate">
                     <i class="filled fas fa-star"></i>
                     <i class="filled fas fa-star"></i>
@@ -19,4 +19,5 @@
         @endforeach
     </div>
 </div>
+@endif
 <!-- End testimonials -->
